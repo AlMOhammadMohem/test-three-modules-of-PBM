d7f8d38d07a5838064f08350f362032893dbb253
AlMOhammadMohem <almohamadmohab@gmail.com>
2026-08-02 22:40:52 +0300

    Create urls.ts

---
+++ constants/urls.ts
@@ -0,0 +1,7 @@
+export const APP_URLS = {
+    base: 'http://20.75.201.176',
+    login: 'http://20.75.201.176/login',
+    payerManagement: 'http://20.75.201.176/payer-management',
+    networkManagement: 'http://20.75.201.176/network-management',
+    approvalManagement: 'http://20.75.201.176/approval-management',
+};
